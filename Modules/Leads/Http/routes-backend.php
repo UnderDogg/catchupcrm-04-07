7496b92d769a7a4c6d4421486bb75f34fce3f575
Niels Drost <nielsdrost7@gmail.com>
2017-04-02 11:20:58 +0200

    Quick commit : Leads Routes backend (for the Leads index)

--- Modules/Leads/Http/routes-backend.php
+++ Modules/Leads/Http/routes-backend.php
@@ -5,32 +5,32 @@ use Illuminate\Routing\Router;
 $router->group([
     'prefix' => 'leads',
     //'middleware' => ['hasPermission'],
-    //'hasPermission' => 'manage@auth_relation',
+    //'hasPermission' => 'manage@auth_lead',
 ], function (Router $router) {
 
-    //'middleware' => 'hasPermission', 'hasPermission' => 'manage.create@auth_relation'
-    $router->group(['prefix' => 'add', 'namespace' => 'Relation'], function (Router $router) {
+    //'middleware' => 'hasPermission', 'hasPermission' => 'manage.create@auth_lead'
+    $router->group(['prefix' => 'add', 'namespace' => 'Lead'], function (Router $router) {
 
         $router->post('/', ['uses' => 'CreateController@postForm']);
-        $router->get('/', ['as' => 'admin.relation.add', 'uses' => 'CreateController@getForm']);
+        $router->get('/', ['as' => 'admin.lead.add', 'uses' => 'CreateController@getForm']);
     });
 
 
     $router->group(['prefix' => 'edit'], function (Router $router) {
         $router->post('/', ['uses' => 'InfoController@postForm']);
-        $router->get('/', ['as' => 'admin.relation.edit', 'uses' => 'InfoController@getForm']);
+        $router->get('/', ['as' => 'admin.lead.edit', 'uses' => 'InfoController@getForm']);
     });
 
 
-    //'middleware' => 'hasPermission', 'hasPermission' => 'manage.read@auth_relation'
+    //'middleware' => 'hasPermission', 'hasPermission' => 'manage.read@auth_lead'
     $router->group(['prefix' => 'view'], function (Router $router) {
-        $router->get('/', ['as' => 'admin.relation.view', 'uses' => 'InfoController@getIndex']);
+        $router->get('/', ['as' => 'admin.lead.view', 'uses' => 'InfoController@getIndex']);
     });
 
-    $router->get('/', ['as' => 'admin.relation.index', 'uses' => function ($relation_id) {
-        return redirect(route('admin.relation.edit', $relation_id));
+    $router->get('/', ['as' => 'admin.lead.index', 'uses' => function ($lead_id) {
+        return redirect(route('admin.lead.edit', $lead_id));
     }]);
 
 
-    $router->get('/', ['as' => 'admin.relation.manager', 'uses' => 'RelationManagerController@relationManager']);
+    $router->get('/', ['as' => 'admin.lead.manager', 'uses' => 'LeadManagerController@leadManager']);
 });
